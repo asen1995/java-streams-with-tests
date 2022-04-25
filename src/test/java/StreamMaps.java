ef01f7cc3d6b71909486f34d0445f5d259a0aa4e
Asen Nikolaev <asen.nikolaev95@abv.bg>
2022-04-25 20:44:41 +0300

    map every element toUpperCase using the map function of streams

---
+++ src/test/java/StreamMaps.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StreamMaps {
+
+    @BeforeAll
+    static void setup() {
+        //setup
+    }
+
+    @DisplayName("mapEveryStringFromStreamAndInvokeUpperCase")
+    @Test
+    public void mapEveryStringFromStreamAndInvokeUpperCase() {
+
+        List<String> upperCaseInit = Arrays.asList("A", "B", "C");
+
+        List<String> upperCaseListAfterMap = Stream.of("a", "b", "c")
+                .map(String::toUpperCase).collect(Collectors.toList());
+        assertNotNull(upperCaseListAfterMap);
+
+        assertArrayEquals(upperCaseInit.toArray(), upperCaseListAfterMap.toArray());
+
+    }
+
+}
