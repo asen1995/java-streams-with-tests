cc2323c39298b0446f8fb88b4424da1e69da9ebd
Asen Nikolaev <asen.nikolaev95@abv.bg>
2022-04-25 16:05:50 +0300

    rename file

--- src/test/java/StreamsBasics.java
+++ src/test/java/StreamsBasics.java
@@ -8,7 +8,7 @@ import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class Streams {
+public class StreamsBasics {
 
     @BeforeAll
     static void setup() {
