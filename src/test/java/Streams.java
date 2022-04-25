9e74374a59b9d99c5671ccace8ca21ec9d02b92f
Asen Nikolaev <asen.nikolaev95@abv.bg>
2022-04-25 15:26:45 +0300

    init empty test structure

---
+++ src/test/java/Streams.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class Streams {
+
+    @BeforeAll
+    static void setup() {
+      //setup
+    }
+
+    @DisplayName("testStructure")
+    @Test
+    void testStructure() {
+    }
+}
