61f71cddc2b88298c58c2eede77591a1ccb60065
Asen Nikolaev <asen.nikolaev95@abv.bg>
2022-04-26 20:47:11 +0300

    streamFilterByCriteriaString test

--- src/test/java/StreamFilter.java
+++ src/test/java/StreamFilter.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-public class StreamFilter {
+public final class StreamFilter {
 
 
     @BeforeAll
@@ -32,6 +32,25 @@ public class StreamFilter {
             assertTrue(number instanceof Integer);
             assertTrue((Integer) number > minNumber);
         });
+    }
+
+    @DisplayName("streamFilterByCriteriaString")
+    @Test
+    public void streamFilterByCriteriaString() {
+
+        final List<Object> objects = Arrays.asList(5, 2, 7, 10, 600, "asen", "peter", 660, 44);
+
+        final Integer minNumber = 10;
+
+        final List<Object> data = objects.stream()
+                .filter(object -> object instanceof String)
+                .collect(Collectors.toList());
+
+        data.forEach(number -> {
+            assertTrue(number instanceof String);
+        });
+
+        assertTrue(data.size() == 2);
     }
 
 
